5f3344f410978f1a03150c8c6fb7030b9781fab8
jaserds <55090872+jaserds@users.noreply.github.com>
2023-12-25 18:01:33 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -16,4 +16,43 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-console.log(112321);
+document.querySelector('.text-blok__btn').addEventListener('click', () => {
+    document.querySelector('.modal-window__container').classList.remove('disable');
+    
+})
+
+document.querySelector('.modal-window__close-btn').addEventListener('click', () => {
+    document.querySelector('.modal-window__container').classList.add('disable');
+})
+
+$('.reviews__slider').slick({
+    dots: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });

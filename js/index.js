e5643578a0b9c08ec5dfb9253d02f88109b0fbfe
jaserds <55090872+jaserds@users.noreply.github.com>
2023-12-27 17:55:16 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -32,26 +32,23 @@ $('.reviews__slider').slick({
     slidesToScroll: 3,
     responsive: [
       {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+          arrows: false,
+        }
+      },
+      {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
+          dots: true,
+          arrows: false,
         }
       }
     ]
